43ca064bf1dc4797a6a70dcb3925c72f2eb115ba
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-10-09 21:51:31 +0300

    JavaScript basics - 1

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
 console.log("Merhaba Kodlama.io");
+
+// camelCasing
+
+let dolarBugun = 27.7;
+let dolarDun = 27.5;
+dolarDun = "27.5";
+
+{
+  let dolarDun = 27.6;
+}
+console.log(dolarDun);
+
+const euroDun = 29.2;
+// euroDun = 29.3;
+console.log(euroDun);
+
+// Array
+let konutKredileri = [
+  "Konut Kredisi",
+  "Emlak Konut Kredisi",
+  "Kamu Konut Kredisi",
+  "Özel Konut Kredisi",
+];
+
+for (let i = 0; i < konutKredileri.length; i++) {
+  console.log(konutKredileri[i]);
+}
