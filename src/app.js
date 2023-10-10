19282a1144a441fb7b06e303230529c1630d0d01
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-10-10 20:05:27 +0300

    JavaScript basics - 2

--- src/app.js
+++ src/app.js
@@ -1,28 +1,65 @@
-console.log("Merhaba Kodlama.io");
+// Lesson - 1
+// ----------
 
-// camelCasing
+// console.log("Merhaba Kodlama.io");
 
-let dolarBugun = 27.7;
-let dolarDun = 27.5;
-dolarDun = "27.5";
+// // camelCasing
 
-{
-  let dolarDun = 27.6;
-}
-console.log(dolarDun);
+// let dolarBugun = 27.7;
+// let dolarDun = 27.5;
+// dolarDun = "27.5";
 
-const euroDun = 29.2;
-// euroDun = 29.3;
-console.log(euroDun);
+// {
+//   let dolarDun = 27.6;
+// }
+// console.log(dolarDun);
+
+// const euroDun = 29.2;
+// // euroDun = 29.3;
+// console.log(euroDun);
 
 // Array
-let konutKredileri = [
-  "Konut Kredisi",
-  "Emlak Konut Kredisi",
-  "Kamu Konut Kredisi",
-  "Özel Konut Kredisi",
-];
+// let konutKredileri = [
+//   "Konut Kredisi",
+//   "Emlak Konut Kredisi",
+//   "Kamu Konut Kredisi",
+//   "Özel Konut Kredisi",
+// ];
 
-for (let i = 0; i < konutKredileri.length; i++) {
-  console.log(konutKredileri[i]);
-}
+// for (let i = 0; i < konutKredileri.length; i++) {
+//   console.log(konutKredileri[i]);
+// }
+
+// Assignment - 1 (Java & React - 10. Gün - JavaScript & ES6)
+// --------------
+
+// var sayi1 = 10;
+// var sayi1 = "Engin Demiroğ";
+// console.log(sayi1);
+
+// you can't
+// let sayi1 = 10;
+// let sayi1 = "Engin Demiroğ";
+
+// let sayi1 = 10;
+// sayi1 = "Engin Demiroğ";
+// let student = { id: 1, name: "Engin" };
+// console.log(student);
+
+// function save(puan = 10, student) {
+//   console.log(`${student.name} - ${puan}`);
+// }
+// ilk parametreyi default geçmek için:
+// save(undefined, student); // output: Engin - 10
+
+// let students = ["Engin Demiroğ", "Halit Kalaycı", "Engin Toprak", "Büşra"];
+// console.log(students);
+
+// let students2 = [
+//   student,
+//   { id: 2, name: "Halit" },
+//   "Ankara",
+//   { city: "İstanbul" },
+// ];
+// console.log(students2);
+// ---------------------------
