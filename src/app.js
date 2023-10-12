e75a7961645f2b8d78be116fc5b3b0a8d390dcb3
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-10-13 01:46:06 +0300

    Assignment - 2 (Prime number, Amicable numbers, Perfect number)

--- src/app.js
+++ src/app.js
@@ -63,3 +63,95 @@
 // ];
 // console.log(students2);
 // ---------------------------
+
+// Assignment - 2
+
+// 1- JavaScript ile istediğiniz kadar sayı parametre gönderebileceğiniz bir fonksiyon yazınız. Bu fonksiyona gönderdiğiniz her sayı için çıktı olarak asal olup olmadığını yazınız. (Araştırma konusu : şart blokları : if)
+
+let findPrimeNumbers = (...numbers) => {
+  let primeNumbers = [];
+  let counter;
+
+  for (let index = 0; index < numbers.length; index++) {
+    counter = 0;
+    for (let i = 1; i <= numbers[index]; i++) {
+      if (numbers[index] % i === 0 && counter <= 2) {
+        counter++;
+      } else if (counter > 2) {
+        break;
+      }
+    }
+    if (counter === 2) {
+      primeNumbers.push(numbers[index]);
+    }
+  }
+  return primeNumbers;
+};
+
+let getNumbers = (limit) => {
+  let numbers = [];
+  for (let i = 1; i <= limit; i++) {
+    numbers.push(i);
+  }
+  return numbers;
+};
+
+// console.log(findPrimeNumbers(...getNumbers(100)));
+
+// 2- Parametre olarak girilen iki sayının arkadaş sayılar olup olmadığını bulan programı yazınız.
+
+let areAmicableNumbers = (firstNumber, secondNumber) => {
+  let firstNumbersDivisorsSum = getDivisors(firstNumber).reduce(
+    (acc, n) => acc + n,
+    0
+  );
+  let secondNumbersDivisorsSum = getDivisors(secondNumber).reduce(
+    (acc, n) => acc + n,
+    0
+  );
+
+  if (
+    firstNumbersDivisorsSum - firstNumber === secondNumber &&
+    secondNumbersDivisorsSum - secondNumber === firstNumber
+  ) {
+    console.log(
+      firstNumbersDivisorsSum - firstNumber,
+      secondNumbersDivisorsSum - secondNumber
+    );
+  }
+};
+
+let getDivisors = (number) => {
+  let divisors = [];
+  for (let i = 1; i <= number; i++) {
+    if (number % i === 0) {
+      divisors.push(i);
+    }
+  }
+  return divisors;
+};
+
+// areAmicableNumbers(284, 220);
+
+// 3- 1000'e kadarki tüm mükemmel sayıları listeleyen programı yazınız.
+
+let perfectNumbers = (limit) => {
+  let perfectNumbers = [];
+  let sum = 0;
+
+  getNumbers(limit).forEach((number) => {
+    sum = getDivisors(number).reduce((acc, n) => acc + n, 0);
+
+    if (sum / 2 === number) {
+      perfectNumbers.push(number);
+    }
+    // sum = 0;
+  });
+  return perfectNumbers;
+};
+
+// console.log(perfectNumbers(1000));
+
+// 4- 1000'e kadarki tüm asal sayıları listeleyen programı yazınız.
+
+console.log(findPrimeNumbers(...getNumbers(1000)));
