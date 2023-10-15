d51afd35fc0d18e628d867d4f44898e00fc2b641
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-10-15 03:17:58 +0300

    We improved the project but wrote mostly bad code

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -5,20 +5,35 @@ import {
 } from "../crossCuttingConcerns/logging/logger.js";
 import User from "../models/user.js";
 import UserService from "../services/userService.js";
+import Customer from "../models/customer.js";
 
 console.log("User Component yüklendi");
 
 let logger1 = new ElasticLogger();
 let userService = new UserService(logger1);
 
-let user1 = new User(1, "Engin", "Demiroğ", "Ankara");
-let user2 = new User(2, "Baran", "Gökçekli", "Muğla");
+// let user1 = new User(1, "Engin", "Demiroğ", "Ankara");
+// let user2 = new User(2, "Baran", "Gökçekli", "Muğla");
+// userService.add(user1);
+// userService.add(user2);
 
-userService.add(user1);
-userService.add(user2);
+// console.log("------------");
+userService.load();
 
-console.log(userService.list());
-console.log(userService.getById(1));
+let customerToAdd = new Customer(7, "Seda", "Yılmaz", "Ankara", "26a");
+// prototype
+customerToAdd.type = "customer";
+userService.add(customerToAdd);
+
+// console.log(userService.customers);
+// console.log(userService.employees);
+console.log(userService.errors);
+
+console.log(userService.listCustomers());
+console.log(userService.listEmployees());
+// console.log(userService.getCustomerById(1));
+console.log("-----sorted-----");
+console.log(userService.getCustomersSorted());
 
 // let customer = { id: 1, firstName: "Engin" };
 // customer.lastName = "Demiroğ"; // prototyping
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,6 @@
+import { users } from "../data/users.js";
+import DataError from "../models/dataError.js";
+
 export default class UserService {
   // export: dışarıdan import edilebilir
   // default: js file-based old. için ben userService.js'i import ettiğimde default olarak UserService'i import et
@@ -5,20 +8,124 @@ export default class UserService {
   // You can export many variables from the same file, 'default' is used only once in the whole file 👉🏼 to let you import this default variable outside without using brackets {}
 
   constructor(loggerService) {
-    this.users = []; // prototyping
+    this.customers = [];
+    this.employees = [];
+    this.errors = [];
     this.loggerService = loggerService;
   }
 
+  load() {
+    for (const user of users) {
+      switch (user.type) {
+        case "customer":
+          if (!this.checkCustomerValidityForErrors(user)) {
+            this.customers.push(user);
+          }
+          break;
+        case "employee":
+          if (!this.checkEmployeeValidityForErrors(user)) {
+            this.employees.push(user);
+          }
+          break;
+        default:
+          this.errors.push(new DataError("Wrong user type.", user));
+          break;
+      }
+    }
+  }
+
+  // react tarafında formik-yup kütüphaneleri bunları(validation) hallediyor
+  checkCustomerValidityForErrors(user) {
+    let requiredFields = "id firstName lastName age city".split(" ");
+    let hasErrors = false;
+    for (const field of requiredFields) {
+      if (!user[field]) {
+        hasErrors = true;
+        this.errors.push(
+          new DataError(`Validation problem. ${field} is required.`, user)
+        );
+      }
+    }
+
+    if (Number.isNaN(Number.parseInt(+user.age))) {
+      hasErrors = true;
+      this.errors.push(
+        new DataError(`Validation problem. ${user.age} is not an age.`, user)
+      );
+    }
+
+    return hasErrors;
+  }
+
+  checkEmployeeValidityForErrors(user) {
+    let requiredFields = "id firstName lastName age city salary".split(" ");
+    let hasErrors = false;
+    for (const field of requiredFields) {
+      if (!user[field]) {
+        hasErrors = true;
+        this.errors.push(
+          new DataError(`Validation problem. ${field} is required.`, user)
+        );
+      }
+    }
+
+    if (Number.isNaN(Number.parseInt(+user.age))) {
+      hasErrors = true;
+      this.errors.push(
+        new DataError(`Validation problem. ${user.age} is not an age.`, user)
+      );
+    }
+
+    return hasErrors;
+  }
+
   add(user) {
-    this.users.push(user);
+    switch (user.type) {
+      case "customer":
+        if (!this.checkCustomerValidityForErrors(user)) {
+          this.customers.push(user);
+        }
+        break;
+      case "employee":
+        if (!this.checkEmployeeValidityForErrors(user)) {
+          this.employees.push(user);
+        }
+        break;
+      default:
+        this.errors.push(
+          new DataError("This user can not be added. Wrong user type.", user)
+        );
+        break;
+    }
     this.loggerService.log(user);
   }
 
-  list() {
-    return this.users;
+  listCustomers() {
+    return this.customers;
   }
 
-  getById(id) {
-    return this.users.find((u) => u.id === id);
+  listEmployees() {
+    return this.employees;
+  }
+
+  getCustomerById(id) {
+    return this.customers.find((c) => c.id === id);
+  }
+
+  getEmployeeById(id) {
+    return this.employees.find((e) => e.id === id);
+  }
+
+  getCustomersSorted() {
+    return this.customers.sort((customer1, customer2) => {
+      if (customer1.firstName > customer2.firstName) {
+        // buralar firstName yerine dinamik olsun
+        return 1;
+      } else if (customer1.firstName === customer2.firstName) {
+        return 0;
+      } else {
+        return -1;
+      }
+    });
   }
 }
