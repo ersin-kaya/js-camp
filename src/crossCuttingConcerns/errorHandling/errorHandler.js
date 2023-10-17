2c8860df426bbf17b953a26c8bde2ce46ca9d102
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-10-18 02:35:25 +0300

    Update ErrorHandler

--- src/crossCuttingConcerns/errorHandling/errorHandler.js
+++ src/crossCuttingConcerns/errorHandling/errorHandler.js
@@ -5,9 +5,23 @@ export default class ErrorHandler {
     this.#errors = [];
   }
 
-  setError(data = undefined, message = undefined) {
-    if (!(data === undefined && message === undefined)) {
-      this.#errors.push({ data: data, message: message });
+  setError(success = undefined, message = undefined) {
+    if (
+      !((success === undefined || success === true) && message === undefined)
+    ) {
+      this.#errors.push({ success: success, message: message });
+    }
+  }
+
+  setErrorWithData(success = undefined, message = undefined, data = undefined) {
+    if (
+      !(
+        (success === undefined || success === true) &&
+        message === undefined &&
+        data === undefined
+      )
+    ) {
+      this.#errors.push({ success: success, message: message, data: data });
     }
   }
 
