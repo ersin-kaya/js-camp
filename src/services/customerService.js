3f647c72ef36e0c9db33cb393518fc49eb3d07a8
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-10-23 10:58:25 +0300

    Update CustomerService

--- src/services/customerService.js
+++ src/services/customerService.js
@@ -1,7 +1,55 @@
+import BusinessRules from "../utilities/business/businessRules.js";
+import ErrorDataResult from "../utilities/results/errorDataResult.js";
+import ErrorResult from "../utilities/results/errorResult.js";
+import SuccessResult from "../utilities/results/successResult.js";
 import EntityServiceBase from "./entityServiceBase.js";
 
 export default class CustomerService extends EntityServiceBase {
   constructor(loggerService, errorHandler) {
     super(loggerService, errorHandler);
+  }
+
+  add(customer) {
+    let businessRules = new BusinessRules();
+    let result = businessRules.run(this.checkAge(customer.age));
+
+    if (result === null) {
+      super.add(customer);
+    } else {
+      this.errorHandler.setErrorWithData(
+        result.success,
+        result.message,
+        customer
+      );
+    }
+  }
+
+  checkAge(age) {
+    if (this.isItAnInteger(age) && age >= 18 && age <= 65) {
+      return new SuccessResult();
+    }
+    return new ErrorResult("This value is not an age.");
+  }
+
+  checkRequiredFields(customer, requiredFields) {
+    let hasErrors = false;
+
+    if (requiredFields !== undefined) {
+      for (const field of requiredFields.split(",")) {
+        if (this.isThereNoData(customer[field]).success) {
+          hasErrors = true;
+          this.errorHandler.setErrorWithData(
+            undefined,
+            `Validation problem. ${field} is required.`,
+            customer
+          );
+        }
+      }
+    }
+
+    if (!hasErrors) {
+      return new SuccessResult();
+    }
+    return new ErrorResult();
   }
 }
